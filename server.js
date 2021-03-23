2ca53d71271746a6a9b1ea3088cb45b22c025b05
nhatpham1997 <nhatpham1997ht@gmail.com>
2021-03-24 00:18:54 +0700

    create order

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const shortid = require('shortid');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const shortid = require("shortid");
 
 const app = express();
 app.use(bodyParser.json());
@@ -16,7 +16,8 @@ const Product = mongoose.model(
     "products",
     new mongoose.Schema({
         _id: {
-            type: String, default: shortid.generate
+            type: String,
+            default: shortid.generate,
         },
         title: String,
         description: String,
@@ -26,21 +27,62 @@ const Product = mongoose.model(
     })
 );
 
-app.get("/api/products",async (req, res) => {
+app.get("/api/products", async (req, res) => {
     const products = await Product.find({});
     res.send(products);
 });
 
-app.post("/api/products", async(req,res)=>{
+app.post("/api/products", async (req, res) => {
     const newProduct = new Product(req.body);
     const savedProduct = await newProduct.save();
     res.send(savedProduct);
 });
 
-app.delete("/api/products/:id", async(req, res) => {
-    const deletedProduct = await Product.findByIdAndDelete(req.params.id)
-    res.send(deleted)
-})
+app.delete("/api/products/:id", async (req, res) => {
+    const deletedProduct = await Product.findByIdAndDelete(req.params.id);
+    res.send(deleted);
+});
+
+const Order = mongoose.model(
+    "order",
+    new mongoose.Schema(
+        {
+            _id: {
+                type: String,
+                default: shortid.generate,
+            },
+            email: String,
+            name: String,
+            address: String,
+            total: Number,
+            cartItems: [
+                {
+                    _id: String,
+                    title: String,
+                    price: Number,
+                    count: Number,
+                },
+            ],
+        },
+        {
+            timestamps: true,
+        }
+    )
+);
+
+app.post("/api/orders", async (req, res) => {
+    if (
+        !req.body.name ||
+        !req.body.email ||
+        !req.body.address ||
+        !req.body.total ||
+        !req.body.cartItems
+    ) {
+        return res.send({ message: "Data is required." });
+    }
+    const order = await Order(req.body).save();
+    res.send(order);
+});
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log("serve at http://localhost:5000"))
+app.listen(port, () => console.log("serve at http://localhost:5000"));
